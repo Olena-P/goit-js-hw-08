9dc6a93d4f709670018ddab872e2bf2b36ece405
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2023-09-24 15:15:49 +0300

    Added lodash trottle to video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,18 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const videoPlayer = document.getElementById('vimeo-player');
 const player = new Player(videoPlayer);
-player.on('timeupdate', function (data) {
+
+const updateLocalStorage = throttle(function (data) {
   const currentTime = data.seconds;
   localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000);
+
+player.on('timeupdate', function (data) {
+  updateLocalStorage(data);
 });
+
 const savedTime = localStorage.getItem('videoplayer-current-time');
 if (savedTime !== null) {
   player.setCurrentTime(parseFloat(savedTime));
